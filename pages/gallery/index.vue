0f7f2226a4cccde288850b914e21f225f1f88918
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-12-14 21:11:16 +0530

    updated gallery page

---
+++ pages/gallery/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>    
+   <Gallery />
+  </div>
+
+</template>
+
+<script setup>
+definePageMeta({
+  //colorMode: 'system',
+  layout: "main",
+})
+const { data: color } = ref('white')
+const { data: services } = await useAsyncData('services-list', () => queryContent('services')
+  .only([
+    "title",
+    "description",
+    "coverimage",
+    "author",
+    "date",
+    "_path",
+    "tags",
+    "type",
+  ])
+  .find())
+</script>
