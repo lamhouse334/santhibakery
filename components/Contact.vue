1e8e8ee1975315bc6d53e22e3dd5b0b2e6b66bee
Infant-Supriya <infantsupriya@gmail.com>
2022-12-14 16:20:20 +0530

    update pages

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,45 +4,48 @@
 
           <div class="mx-auto max-w-7xl">
               <div class="flex flex-col lg:flex-row">
-                  <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
-                      <div class="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
-                          <div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
-                              <div class="relative">
-                                  <p class="mb-2 font-medium text-gray-700 uppercase">Work smarter</p>
-                                  <h2 class="text-5xl font-bold text-gray-900 xl:text-6xl">Features to help you work smarter</h2>
-                              </div>
-                              <p class="text-2xl text-gray-700">We've created a simple formula to follow in order to gain more out of your business and your application.</p>
-                              <a href="#_" class="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Get Started Today</a>
-                          </div>
-                      </div>
-                  </div>
-
                   <div class="w-full bg-white lg:w-6/12 xl:w-5/12">
-                      <div class="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
-                          <h4 class="w-full text-3xl font-bold">Signup</h4>
-                          <p class="text-lg text-gray-500">or, if you have an account you can <a href="#_" class="text-blue-600 underline">sign in</a></p>
-                          <div class="relative w-full mt-10 space-y-8">
-                              <div class="relative">
-                                  <label class="font-medium text-gray-900">Name</label>
-                                  <input type="text" name="first_name" id="first_name" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Name">
-                              </div>
-                              <div class="relative">
-                                  <label class="font-medium text-gray-900">Email</label>
-                                  <input type="text" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Email Address">
-                              </div>
-                              <div class="relative">
-                                  <label class="font-medium text-gray-900">Phone no</label>
-                                  <input type="password" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Password">
-                              </div>
-                              <div class="relative">
-                                  <a href="#_" class="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Create Account</a>
-                                  <a href="#_" class="inline-block w-full px-5 py-4 mt-3 text-lg font-bold text-center text-gray-900 transition duration-200 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 ease">Sign up with Google</a>
-                              </div>
-                          </div>
-                      </div>
+                    
+                      
+                      <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+
+                        <div>
+                        <div class="contact-form-wrapper d-flex justify-content-center">
+                            <form action="#" class="contact-form">
+                            <h5 class="title">Contact us</h5>
+                            <div>
+                                <input type="text" class="form-control rounded border-white mb-3 form-input" id="name" placeholder="Name" required>
+                            </div>
+                            <div>
+                                <input type="email" class="form-control rounded border-white mb-3 form-input" placeholder="Email" required>
+                            </div>
+                            <div>
+                                <input type="integer" class="form-control rounded border-white mb-3 form-input" placeholder="Phone no" required>
+                            </div>
+                            <div>
+                                <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5"  placeholder="Message" required></textarea>
+                            </div>
+                            <div class="submit-button-wrapper">
+                                <input type="submit" value="Submit">
+                            </div>
+                            </form>
+                        </div>
+                        </div>
                   </div>
+                  <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-white">
+                    <div class="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
+                        <div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+                            <div class="relative">
+                                <h2 class="text-5xl font-bold text-primary-900 xl:text-6xl uppercase">How to order</h2>
+                            </div>
+                            <p class="text-xl text-gray-700">We heartily welcome you to santhibakery.com. When you choose your favourite bakery item order through our website. We process your order and prepare fresh items according to your requirement. The processing time differs in accordance with the products you order. Once order is confirmed, we advice you the time delivery.The moment your product is ready, we make immediate arrangements to deliver same to your doorstep hot and fresh. You can also order to phone No.8148176514. Once your order is confirmed, we do on order through our website.</p>
+                        </div>
+                    </div>
+                </div>
               </div>
           </div>
 
       </section>
 </template>
+<style scoped src="@/assets/css/Contact.css"></style>
+<style scoped src="@/assets/css/bootstrap.css"></style>
---
+++ pages/Gallery.vue
@@ -0,0 +1,66 @@
+<template>
+    <section class="blog">
+        <div class="container">
+          <!-- header of section -->
+          <div class="blog-head text-center">
+            <h2>Gallery</h2>
+            <h6>Our Products</h6>
+          </div>
+
+          <!-- blog items -->
+          <div class="row">
+            <div class="col-md-6 col-lg-4">
+              <div class="item animated wow fadeIn">
+                  <img src="https://res.cloudinary.com/lamcom/image/upload/v1670959272/santhibakery/home/Services/pexels-quang-nguyen-vinh-2144200_1_nu2edl.jpg" alt="">
+                  <div class="overlay top-overlay">
+    <div class="text">Cakes</div>
+  </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="item animated wow fadeIn">
+                  <img src="https://res.cloudinary.com/lamcom/image/upload/v1670959439/santhibakery/home/Services/pexels-pixabay-36756_1_g8sgne.jpg" alt="">
+                  <div class="overlay top-overlay">
+    <div class="text">Puffs</div>
+  </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="item animated wow fadeIn">
+                  <img src="https://res.cloudinary.com/lamcom/image/upload/v1670959795/santhibakery/home/Services/pexels-almendra-l%C3%B3pez-varela-8520897_1_y3kje3.jpg" alt="">
+                  <div class="overlay top-overlay">
+    <div class="text">Macarons</div>
+  </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="item animated wow fadeIn">
+                  <img src="https://res.cloudinary.com/lamcom/image/upload/v1670959951/santhibakery/home/Services/pexels-pure-punjabi-7182054_1_pmcppo.jpg" alt="">
+                  <div class="overlay top-overlay">
+    <div class="text">Sweets</div>
+  </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="item animated wow fadeIn">
+                  <img src="https://res.cloudinary.com/lamcom/image/upload/v1670960125/santhibakery/home/Services/pexels-pixabay-33715_cad8t9.jpg" alt="">
+                  <div class="overlay top-overlay">
+    <div class="text">Chocolates</div>
+  </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="item animated wow fadeIn">
+                  <img src="https://res.cloudinary.com/lamcom/image/upload/v1670960296/santhibakery/home/Services/pexels-cats-coming-674574_1_ixr3ae.jpg" alt="">
+                  <div class="overlay top-overlay">
+    <div class="text">Food</div>
+  </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+</template>
+<style scoped src="@/assets/css/gallery.css"></style>
+<style scoped src="@/assets/css/bootstrap.css"></style>
