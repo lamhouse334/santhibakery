3d8aacd7c4ece5351b3c67e21e2035643257a0ed
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-12-15 16:28:52 +0530

    updated contact

--- components/Header.vue
+++ components/Header.vue
@@ -57,7 +57,7 @@
 			<!-- <NuxtLink to="#"
 				class="px-5 py-2 text-sm  ring-2 ring-primary-500 hover:bg-primary-700 ring-inset text-white rounded-full transition-all duration-300">
 				Sign in</NuxtLink> -->
-			<NuxtLink to="#" @click="useNuxtApp().$bus.$emit('evtShowContactSales')"
+			<NuxtLink to="contact"
 				class="px-5 py-2 text-sm bg-primary-500 text-white rounded-full hover:bg-primary-700 transition-all duration-300">
 				Contact us</NuxtLink>
 			<!--<NuxtLink to="#" @click="useNuxtApp().$bus.$emit('evtShowContactSales')"
