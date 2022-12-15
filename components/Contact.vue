ad593bc1ae0d9c3c77088737176cf5718ca33282
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-12-15 16:08:07 +0530

    updated the phone number

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,51 +1,115 @@
 <template>
-    <!-- Section 2 -->
-      <section class="w-full bg-white">
+  <!-- Section 2 -->
+  <section class="w-full bg-white">
+    <div class="mx-auto max-w-7xl">
+      <div class="flex flex-col lg:flex-row">
+        <div class="w-full bg-white lg:w-6/12 xl:w-5/12">
+          <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
 
-          <div class="mx-auto max-w-7xl">
-              <div class="flex flex-col lg:flex-row">
-                  <div class="w-full bg-white lg:w-6/12 xl:w-5/12">
-                    
-                      
-                      <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-
-                        <div>
-                        <div class="contact-form-wrapper d-flex justify-content-center">
-                            <form action="#" class="contact-form">
-                            <h5 class="title">Contact us</h5>
-                            <div>
-                                <input type="text" class="form-control rounded border-white mb-3 form-input" id="name" placeholder="Name" required>
-                            </div>
-                            <div>
-                                <input type="email" class="form-control rounded border-white mb-3 form-input" placeholder="Email" required>
-                            </div>
-                            <div>
-                                <input type="integer" class="form-control rounded border-white mb-3 form-input" placeholder="Phone no" required>
-                            </div>
-                            <div>
-                                <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5"  placeholder="Message" required></textarea>
-                            </div>
-                            <div class="submit-button-wrapper">
-                                <input type="submit" value="Submit">
-                            </div>
-                            </form>
-                        </div>
-                        </div>
-                  </div>
-                  <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-white">
-                    <div class="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
-                        <div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
-                            <div class="relative">
-                                <h2 class="text-5xl font-bold text-primary-900 xl:text-6xl uppercase">How to order</h2>
-                            </div>
-                            <p class="text-xl text-gray-700">We heartily welcome you to santhibakery.com. When you choose your favourite bakery item order through our website. We process your order and prepare fresh items according to your requirement. The processing time differs in accordance with the products you order. Once order is confirmed, we advice you the time delivery.The moment your product is ready, we make immediate arrangements to deliver same to your doorstep hot and fresh. You can also order to phone No.8148176514. Once your order is confirmed, we do on order through our website.</p>
-                        </div>
-                    </div>
+          <div>
+            <div class="contact-form-wrapper d-flex justify-content-center">
+              <form action="#" class="contact-form">
+                <h5 class="title">Contact us</h5>
+                <div>
+                  <input
+                    type="text"
+                    class="form-control rounded border-white mb-3 form-input"
+                    id="name"
+                    placeholder="Name"
+                    required
+                  />
                 </div>
-              </div>
+                <div>
+                  <input
+                    type="email"
+                    class="form-control rounded border-white mb-3 form-input"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    type="integer"
+                    class="form-control rounded border-white mb-3 form-input"
+                    placeholder="Phone no"
+                    required
+                  />
+                </div>
+                <div>
+                  <textarea
+                    id="message"
+                    class="
+                      form-control
+                      rounded
+                      border-white
+                      mb-3
+                      form-text-area
+                    "
+                    rows="5"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </div>
+                <div class="submit-button-wrapper">
+                  <input type="submit" value="Submit" />
+                </div>
+              </form>
+            </div>
           </div>
-
-      </section>
+        </div>
+        <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-white">
+          <div
+            class="
+              relative
+              flex flex-col
+              items-center
+              justify-center
+              w-full
+              h-full
+              px-10
+              my-20
+              lg:px-16 lg:my-0
+            "
+          >
+            <div
+              class="
+                flex flex-col
+                items-start
+                space-y-8
+                tracking-tight
+                lg:max-w-3xl
+              "
+            >
+              <div class="relative">
+                <h2
+                  class="
+                    text-5xl
+                    font-bold
+                    text-primary-900
+                    xl:text-6xl
+                    uppercase
+                  "
+                >
+                  How to order
+                </h2>
+              </div>
+              <p class="text-xl text-gray-700">
+                We heartily welcome you to santhibakery.com. When you choose
+                your favourite bakery item order through our website. We process
+                your order and prepare fresh items according to your
+                requirement. The processing time differs in accordance with the
+                products you order. Once order is confirmed, we advice you the
+                time delivery.The moment your product is ready, we make
+                immediate arrangements to deliver same to your doorstep hot and
+                fresh. You can also order to phone No.97883 70003. Once your
+                order is confirmed, we do on order through our website.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <style scoped src="@/assets/css/contact.css"></style>
 <style scoped src="@/assets/css/bootstrap.min.css"></style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,55 @@
 <template>
-<main
-		class="mx-auto bg-gradient-to-r from-primary-800 via-primary-900 to-primary-800 ... lg:max-w-8xl px-5 lg:px-0 pt-14 lg:pt-12 aboutbg">
-		<h1 class="capitalize font-semibold text-[38px] lg:text-7xl text-center leading-snug text-white">			
-			
-			<span class="font-extrabold bg-clip-text text-primary-50 uppercase">About</span>
-		</h1>
-		
-		<div class="text-center p-10 flex items-center justify-center space-x-4">
-			<a href="contact"
-				class="px-5 py-2 text-sm  ring-2 ring-primary-500 hover:bg-primary-700 ring-inset text-white rounded-full transition-all duration-300">
-				Contact us</a>
-			<!--<div class="relative group">
+  <main
+    class="
+      mx-auto
+      bg-gradient-to-r
+      from-primary-800
+      via-primary-900
+      to-primary-800
+      ...
+      lg:max-w-8xl
+      px-5
+      lg:px-0
+      pt-14
+      lg:pt-12
+      aboutbg
+    "
+  >
+    <h1
+      class="
+        capitalize
+        font-semibold
+        text-[38px]
+        lg:text-7xl
+        text-center
+        leading-snug
+        text-white
+      "
+    >
+      <span class="font-extrabold bg-clip-text text-primary-50 uppercase"
+        >About</span
+      >
+    </h1>
+
+    <div class="text-center p-10 flex items-center justify-center space-x-4">
+      <a
+        href="contact"
+        class="
+          px-5
+          py-2
+          text-sm
+          ring-2 ring-primary-500
+          hover:bg-primary-700
+          ring-inset
+          text-white
+          rounded-full
+          transition-all
+          duration-300
+        "
+      >
+        Contact us</a
+      >
+      <!--<div class="relative group">
 				<div
 					class="absolute -inset-0.5 bg-gradient-to-r from-secondary-600 to-primary-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt">
 				</div>
@@ -34,20 +73,52 @@
 					>Take Tour</span
 				>
 			</NuxtLink>-->
-		</div>
-	</main>
-  <div class="max-w-7xl mx-auto mt-16 my-16 lg:grid lg:grid-cols-2 md:flex md:flex-col">
+    </div>
+  </main>
+  <div
+    class="
+      max-w-7xl
+      mx-auto
+      mt-16
+      my-16
+      lg:grid lg:grid-cols-2
+      md:flex md:flex-col
+    "
+  >
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-     <img src="https://res.cloudinary.com/lamcom/image/upload/v1670772073/santhibakery/home/Web_Photo_Editor_1_plkgqq.jpg">
-     <div class="box-content bg-primary-900 mt-2 w-full h-25 rounded-lg">
-     <p class="text-center p-10 text-white font-semibold text-xl ">We Bake to your taste</p>
-     </div>
+      <img
+        src="https://res.cloudinary.com/lamcom/image/upload/v1670772073/santhibakery/home/Web_Photo_Editor_1_plkgqq.jpg"
+      />
+      <div class="box-content bg-primary-900 mt-2 w-full h-25 rounded-lg">
+        <p class="text-center p-10 text-white font-semibold text-xl">
+          We Bake to your taste
+        </p>
       </div>
+    </div>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="lg:text-center md:text-center">
-        <h2 class="text-base text-primary-700 font-semibold tracking-wide uppercase">About Us</h2>
-        <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-         OUR MOTTO
+        <h2
+          class="
+            text-base text-primary-700
+            font-semibold
+            tracking-wide
+            uppercase
+          "
+        >
+          About Us
+        </h2>
+        <p
+          class="
+            mt-2
+            text-3xl
+            leading-8
+            font-extrabold
+            tracking-tight
+            text-gray-900
+            sm:text-4xl
+          "
+        >
+          OUR MOTTO
         </p>
         <!-- <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto md:mx-auto">
           LAM House symbolises Brilliance, Reliability and Integrity
@@ -58,14 +129,32 @@
         <dl class="space-y-10 md:space-y-0 md:grid md:gap-y-10">
           <div v-for="about in about" :key="about.name" class="relative">
             <dt>
-              <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-900 text-white">
-                <component :is="about.icon" class="h-6 w-6" aria-hidden="true" />
+              <div
+                class="
+                  absolute
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-md
+                  bg-primary-900
+                  text-white
+                "
+              >
+                <component
+                  :is="about.icon"
+                  class="h-6 w-6"
+                  aria-hidden="true"
+                />
               </div>
-              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">{{ about.name }}</p>
-            </dt>            
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                {{ about.name }}
+              </p>
+            </dt>
           </div>
         </dl>
-      </div>      
+      </div>
     </div>
   </div>
 
@@ -91,59 +180,60 @@
         </dl>
       </div> 
    </div> -->
-
-   
-  
-  
 </template>
 
 <script>
-import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon, ArrowCircleRightIcon } from '@heroicons/vue/outline/index.js'
+import {
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+  ScaleIcon,
+  ArrowCircleRightIcon,
+} from "@heroicons/vue/outline/index.js";
 definePageMeta({
   //colorMode: 'system',
   layout: "main",
-})
+});
 const about = [
   {
-    name: 'We Strive to bring out tasty and palatable bakery items which include cakes,buns,puffs,etc.,to the entire satisfaction of our customers.',    
+    name: "We Strive to bring out tasty and palatable bakery items which include cakes,buns,puffs,etc.,to the entire satisfaction of our customers.",
     icon: ArrowCircleRightIcon,
   },
   {
-    name: "SANTHI BAKERY plans to create innovations and professional expertise to meet the customer's immediate and future requirements.Our food mall is A/C and NON A/C, providing 100% vegetarian food items.",    
+    name: "SANTHI BAKERY plans to create innovations and professional expertise to meet the customer's immediate and future requirements.Our food mall is A/C and NON A/C, providing 100% vegetarian food items.",
     icon: ArrowCircleRightIcon,
   },
   {
-    name: "We value our customer's patronage for the most clean, hygiene and economical quality products to suite every family's taste. Consistency in the products is our speciality.",    
+    name: "We value our customer's patronage for the most clean, hygiene and economical quality products to suite every family's taste. Consistency in the products is our speciality.",
     icon: ArrowCircleRightIcon,
   },
   {
-    name: "We also arrange kitty parties, Birthday, Anniversary and other events to the customer's fullest satisfaction. We also use the latest equipments and technology in the production of our bakery and food items.",   
+    name: "We also arrange kitty parties, Birthday, Anniversary and other events to the customer's fullest satisfaction. We also use the latest equipments and technology in the production of our bakery and food items.",
     icon: ArrowCircleRightIcon,
   },
   {
-    name: "Customers satisfaction is our main motto. Please contact us at 9965838485.We remain always at your service.",   
+    name: "Customers satisfaction is our main motto. Please contact us at 97883 70003.We remain always at your service.",
     icon: ArrowCircleRightIcon,
   },
-]
+];
 
 // // const legacy = [
 // //   {
-// //     name: 'The name LAM House is inspired by and dedicated to my parents and family. LAM is an acronym for Life, Art and Multimedia.',    
+// //     name: 'The name LAM House is inspired by and dedicated to my parents and family. LAM is an acronym for Life, Art and Multimedia.',
 // //     icon: ArrowCircleRightIcon,
 // //   },
 // //   {
-// //     name: 'Mr. Muthuraman A. as the proprietor of this company brings along with him over 20 years of experience in the fields of animation and graphic design. From managerial roles, to HR roles to administrative details to the financial aspects of a graphic designing and software development firm. In the past years he has grown and groomed our team to reflect these qualities of integrity and dedication in their work.',    
+// //     name: 'Mr. Muthuraman A. as the proprietor of this company brings along with him over 20 years of experience in the fields of animation and graphic design. From managerial roles, to HR roles to administrative details to the financial aspects of a graphic designing and software development firm. In the past years he has grown and groomed our team to reflect these qualities of integrity and dedication in their work.',
 // //     icon: ArrowCircleRightIcon,
-// //   },  
+// //   },
 // ]
 export default {
   setup() {
     return {
       about,
-      // legacy,     
-    }   
+      // legacy,
+    };
   },
- 
-}
+};
 </script>
 <style scoped src="@/assets/css/newstyle.css"></style>
